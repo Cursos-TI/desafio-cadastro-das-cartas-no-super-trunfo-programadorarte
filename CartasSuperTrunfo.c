0a6174a1dc899f641671ead80c425fd0bcb1cd0b
programadorarte <samuelferreirasf3823190@gmail.com>
2025-03-08 18:05:54 +0000

    criei 0 printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,12 +3,12 @@
   int main(){
   printf("desafio trunfo! /n");
 
-  char maranhao
-  char 200
-  char sao luiz
-  int 15000
-  float 100000
-  float 1300
-  int 1000
+  char estado = "maranhao";
+  char numero = 100;
+  char cidade = "são luiz"; 
+  int populacao =  15000;
+  float area =  10000;
+  float pib = 1300;
+  int pomtos = 1000;
     return 0;
 }
