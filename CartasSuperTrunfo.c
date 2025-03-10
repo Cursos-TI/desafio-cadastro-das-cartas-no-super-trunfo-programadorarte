92f8adb95c76c89aee05976647c54f31e431f09a
programadorarte <samuelferreirasf3823190@gmail.com>
2025-03-10 13:21:01 +0000

    novo printf

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,12 +3,37 @@
   int main(){
   printf("desafio trunfo! /n");
 
-  char estado = "maranhao";
+  char estado [19] = "maranhao";
   char numero = 100;
   char cidade = "são luiz"; 
   int populacao =  15000;
   float area =  10000;
   float pib = 1300;
-  int pomtos = 1000;
-    return 0;
+  int pontos = 1000;
+  
+  printf("digite o nome do estado:");
+  scanf("%19s", estado);
+
+   printf("digite a sigla do estado:");
+   scanf("%s", numero);
+
+   printf("digite o nome da cidade:");
+   scanf("%s", cidade);
+   
+   printf("digite um numero:");
+   scanf("%d", &numero);
+
+   printf("digite a populacao:");
+   scanf("%d", &populacao);
+
+   printf("digite a area:");
+   scanf("%f", &area);
+
+   printf("digite o pib:");
+   scanf("%F", &pib);
+
+   printf("digite os pontos:");
+   scanf("%d", &pontos );
+
+   return 0;
 }
